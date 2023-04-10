8af54d4740615a4e507894661abf9b83d2f7a219
Tu nombreSaul <psauljavier6@gmail.com>
2023-04-10 15:17:04 -0700

    Se cargo el proyecto

---
+++ src/views/PostViews.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="postviews">
+      <h1>This is an post page</h1>
+    </div>
+  </template>
+  
+  <script>
+
+  export default {
+    name: 'PostViews',
+    components: {
+    }
+  }
+  </script>
